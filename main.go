3f44edc06ae74a31a3ec96a261b0909124dfda11
Vishnevyy <golubovamary@yandex.ru>
2025-02-03 18:25:39 +0300

    Добавлены POST и GET ручки для работы с сообщениями

--- main.go
+++ main.go
@@ -2,47 +2,48 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-var task string
-
-func GetHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		if task == "" {
-			fmt.Fprintln(w, "hello, no task set")
-		} else {
-			fmt.Fprintf(w, "hello, %s\n", task)
-		}
-	} else {
-		fmt.Fprintln(w, "Поддерживается только метод Get")
-	}
+func CreateMessage(w http.ResponseWriter, r *http.Request) {
+    if r.Method == http.MethodPost {
+        var message Message
+        if err := json.NewDecoder(r.Body).Decode(&message); err != nil {
+            http.Error(w, err.Error(), http.StatusBadRequest)
+            return
+        }
+        if err := DB.Create(&message).Error; err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        w.WriteHeader(http.StatusCreated)
+        json.NewEncoder(w).Encode(message)
+    } else {
+        http.Error(w, "Метод не поддерживается", http.StatusMethodNotAllowed)
+    }
 }
 
-func PostHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		var data map[string]string
-
-		if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		if t, ok := data["task"]; ok {
-			task = t
-			fmt.Fprintf(w, "Task сохранён: %s\n", task)
-		} else {
-			http.Error(w, "Поле 'task' обязательно.", http.StatusBadRequest)
-		}
-	} else {
-		fmt.Fprintln(w, "Поддерживается только метод Post")
-	}
+func GetMessages(w http.ResponseWriter, r *http.Request) {
+    if r.Method == http.MethodGet {
+        var messages []Message
+        DB.Find(&messages)
+        w.Header().Set("Content-Type", "application/json")
+        json.NewEncoder(w).Encode(messages)
+    } else {
+        http.Error(w, "Метод не поддерживается", http.StatusMethodNotAllowed)
+    }
 }
 
 func main() {
-	http.HandleFunc("/get", GetHandler)
-	http.HandleFunc("/post", PostHandler)
+    InitDB()
+    DB.AutoMigrate(&Message{})
 
-	http.ListenAndServe("localhost:8080", nil)
+    router := mux.NewRouter()
+    router.HandleFunc("/api/messages", CreateMessage).Methods("POST")
+    router.HandleFunc("/api/messages", GetMessages).Methods("GET")
+    log.Println("Server is running on port 8080...")
+    http.ListenAndServe(":8080", router)
 }
